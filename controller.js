07b77a281252fb6e7091b86c5e7216a50be7a0fa
hesam <hesamkhanjani1384@gmail.com>
2024-08-21 19:48:09 +0430

    complete the part one(get the url in html)

--- controller.js
+++ controller.js
@@ -1,16 +1,22 @@
 import express from "express"
+import bodyParser from "body-parser"
 const app = express()
 const PORT = process.env.PORT || 1000
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.set("views" , "./view")
 app.set("view engine" , "ejs")
+
 
 app.get('/' , (req,res)=>{
 
     res.render("index" , {message: "hello every one"})
 })
+
+
 app.post('/post' , (req,res)=>{
-    console.log(req.query)
+    console.log(req.body)
 })
 
 
